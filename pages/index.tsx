a1ea399aa2d31e9a0c24168723d4e43dfd64e1cc
wanning <z5371162@unsw.edu.au>
2022-07-17 17:48:58 +1000

    added chakra UI packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,23 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+
+const searchClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
 
 const Home: NextPage = () => {
   return (
-    <div>hello</div>
+    <div>
+      <h1>DigiDirectory</h1>
+      <InstantSearch searchClient={searchClient} indexName="demo_ecommerce">
+        <SearchBox />
+        <Hits />
+      </InstantSearch>
+    </div>
   )
 }
 
