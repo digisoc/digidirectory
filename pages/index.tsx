59db6e01862141c4b592cb8025edfe6c8dcbf341
wanning <z5371162@unsw.edu.au>
2022-07-17 17:59:26 +1000

    added new digi favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
